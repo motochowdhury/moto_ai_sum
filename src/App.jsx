6c35a7a49144cbf7a57da54fc9cc9abb15bbc3af
motochowdhury <motochowdhury@gmail.com>
2023-04-23 12:41:17 +0600

    setting up moto_ai_sum project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from './assets/logo.svg'
+
+const App = () => {
+  return (
+    <div>
+        <img style={{marginLeft: "50px", marginTop: "60px"}} src={logo} alt='logo' />
+    </div>
+  )
+}
+
+export default App
