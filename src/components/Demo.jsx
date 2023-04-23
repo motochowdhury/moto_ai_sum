79a3371679105d950b88daaeaac90ef2fce7598a
motochowdhury <motochowdhury@gmail.com>
2023-04-24 02:29:46 +0600

    form input done with styling

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -5,7 +5,23 @@ import {linkIcon,loader,tick,copy} from '../assets';
 const Demo = () => {
   return (
     <section className="mt-16 w-full max-w-xl ">
-
+      <div className="flex flex-col w-full gap-2">
+        <form className="relative flex justify-center items-center"
+        onSubmit={()=>{}}
+        >
+          <img src={linkIcon} alt="" 
+          className="absolute left-0 my-2 ml-3 w-5"
+          />
+          <input 
+            type="url"
+            value=""
+            onChange={()=>{}}
+            placeholder="Enter a URL"
+            required
+            className="url_input peer"
+          />
+        </form>
+      </div>
     </section >
   )
 }
