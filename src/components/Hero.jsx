a19ad0680fc43ffb26ef0aed3641dc8f02ad96cf
motochowdhury <motochowdhury@gmail.com>
2023-04-23 18:24:36 +0600

    navbar design is running with styling

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,16 @@
-import React from 'react'
-
+import { logo } from '../assets'
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-between items-center w-full'>
+       <img src={logo} alt="" className='w-28' />
+       <button
+       type='button'
+       onClick={()=> window.open('https://google.com')}
+       className='black_btn'
+       >gitHub</button>
+      </nav>
+    </header>
   )
 }
 
