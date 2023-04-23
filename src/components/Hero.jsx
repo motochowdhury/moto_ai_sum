c77698d6894a0a7803ca03ccf90d76f46f2cc828
motochowdhury <motochowdhury@gmail.com>
2023-04-23 18:25:36 +0600

    margin and padding done at nav

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,7 @@ import { logo } from '../assets'
 const Hero = () => {
   return (
     <header className='w-full flex justify-center items-center flex-col'>
-      <nav className='flex justify-between items-center w-full'>
+      <nav className='flex justify-between items-center w-full mb-10 pt-3'>
        <img src={logo} alt="" className='w-28' />
        <button
        type='button'
