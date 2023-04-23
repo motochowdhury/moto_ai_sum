b81ae95cbc638302d2747e92bccebb0e485f5be5
motochowdhury <motochowdhury@gmail.com>
2023-04-24 02:43:24 +0600

    peer btn and input border color changed style by tailwind is done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -22,7 +22,7 @@ const Demo = () => {
           />
           <button 
             type="submit"
-            className="submit_btn"
+            className="submit_btn peer-focus:border-gray-700 peer-focus:text-gray-700"
           >
             ⮌
           </button>
