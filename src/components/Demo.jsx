3c96dc1d7b816a1f756bf4ecfa872dc25e1c98eb
motochowdhury <motochowdhury@gmail.com>
2023-04-25 13:18:33 +0600

    form handling done

--- src/components/Demo.jsx
+++ src/components/Demo.jsx
@@ -3,19 +3,29 @@ import { useState,useEffect } from "react";
 import {linkIcon,loader,tick,copy} from '../assets';
 
 const Demo = () => {
+  const [article,setArticle] = useState({
+    url: '',
+    summary: ''
+  })
+
+  const handleSubmit = async(e) => {
+    alert('submit');
+  }
+
+
   return (
     <section className="mt-16 w-full max-w-xl ">
       <div className="flex flex-col w-full gap-2">
         <form className="relative flex justify-center items-center"
-        onSubmit={()=>{}}
+        onSubmit={handleSubmit}
         >
           <img src={linkIcon} alt="" 
           className="absolute left-0 my-2 ml-3 w-5"
           />
           <input 
             type="url"
-            value=""
-            onChange={()=>{}}
+            value={article.url}
+            onChange={(e)=>setArticle({...article, url: e.target.value})}
             placeholder="Enter a URL"
             required
             className="url_input peer"
