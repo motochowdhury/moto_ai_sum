633772cf48525f6d63b0da50c3a026935804440a
motochowdhury <motochowdhury@gmail.com>
2023-05-04 14:04:31 +0600

    header is set successfully

--- src/services/article.js
+++ src/services/article.js
@@ -1,5 +1,21 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
+const rapidApiKey = import.meta.VITE_RAPID_API_ARTICLE_KEY;
+
 export const articleApi = createApi({
-    reducerPath: "articleApi"
+    reducerPath: "articleApi",
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', rapidApiKey)
+            headers.set('X-RapidAPI-Host', 'https://article-extractor-and-summarizer.p.rapidapi.com/summarize' )
+
+            return headers
+        }
+    }),
+    endpoints: (builder) => ({
+        getSummery: builder.query({
+            query: (params) => `test`
+        })
+    })
 })
--- src/services/store.js
+++ src/services/store.js
@@ -4,7 +4,6 @@ import { articleApi } from "./article";
 
 export const store = configureStore({
     reducer: {[articleApi.reducerPath] : articleApi.reducer},
-    middleware: (getDefaultMiddleware) => {
+    middleware: (getDefaultMiddleware) => 
         getDefaultMiddleware().concat(articleApi.middleware)
-    }
 })
